62caa1a469147c20c20dcc86f0b945697c02c4e7
Dirk Kessler <dirk.kessler@asynchrony.com>
2016-06-21 10:54:57 -0500

    Added Browserify

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp'),
 	gutil = require('gulp-util'),
 	coffee = require('gulp-coffee'),
+	browserify = require('gulp-browserify'),
 	concat = require('gulp-concat');
 
 var coffeeSources = ['components/coffee/tagline.coffee'];
@@ -13,13 +14,14 @@ var jsSources = [
 
 gulp.task('coffee', function() {
 	gulp.src(coffeeSources)
-	.pipe(coffee({bare: true})
-		.on('error', gutil.log))
-	.pipe(gulp.dest('components/scripts'))
+		.pipe(coffee({bare: true})
+			.on('error', gutil.log))
+		.pipe(gulp.dest('components/scripts'))
 });
 
 gulp.task('js', function() {
 	gulp.src(jsSources)
-	.pipe(concat('script.js'))
-	.pipe(gulp.dest('builds/development/js'))
+		.pipe(concat('script.js'))
+		.pipe(browserify())
+		.pipe(gulp.dest('builds/development/js'))
 });
